4aa87e91a493c4ad3ee0d48a99656d9be6ef2f45
EmmyAnieDev <Emaduilzjr1@gmail.com>
2025-01-05 12:14:26 +0100

    Logout and little fixes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,24 +7,18 @@
         <li class="setting_area">
             <a class="setting"><i class="fal fa-cog"></i></a>
             <ul class="drop_menu drop_menu_setting">
-                <li><a href="{{ route('profile.edit') }}">settings</a></li>
-                <li><a href="#">enable dark theme</a></li>
-                <li><a href="#">send feedback</a></li>
-                <li><a href="#">help</a></li>
-                <li><a href="#">app downloads</a></li>
+                <li><a href="{{ route('profile.edit') }}">profile</a></li>
+                <li><a CLASS="text-danger" href="javascript:;" onclick="$('.logout').submit()">logout</a></li>
             </ul>
         </li>
         <li class="user_area">
             <div class="user">
                 <img src="{{ asset('assets/images/user_icon.png') }}" alt="user" class="img-fluid">
             </div>
-            <ul class="drop_menu drop_menu_user">
-                <li><a href="#">settings</a></li>
-                <li><a href="#">enable dark theme</a></li>
-                <li><a href="#">send feedback</a></li>
-                <li><a href="#">help</a></li>
-                <li><a href="#">app downloads</a></li>
-            </ul>
         </li>
+
+        <form action="{{ route('logout') }}" method="POST" class="logout">
+            @csrf
+        </form>
     </ul>
 </header>
